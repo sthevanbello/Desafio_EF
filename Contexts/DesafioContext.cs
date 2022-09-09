f2de5ba6c7a28c22818de9bd276a0ea566665d90
sthevanbello <sthevan.alves@gmail.com>
2022-09-09 14:38:48 -0300

    Classe Context finalizada

---
+++ Contexts/DesafioContext.cs
@@ -0,0 +1,21 @@
+﻿using Desafio_EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio_EF.Contexts
+{
+    public class DesafioContext : DbContext
+    {
+        public DesafioContext(DbContextOptions<DesafioContext> options) : base(options)
+        {
+        }
+        // Acesso ao banco de dados
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<TipoUsuario> TipoUsuario { get; set; }
+        public DbSet<Paciente> Paciente { get; set; }
+        public DbSet<Medico> Medico { get; set; }
+        public DbSet<Especialidade> Especialidade { get; set; }
+        public DbSet<Consulta> Consulta { get; set; } // Classe de relacionamento entre Médico e Paciente
+
+
+    }
+}
