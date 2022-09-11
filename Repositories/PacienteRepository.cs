9d5a5d9e66b47f4fba3d6949206d2dfea5eade92
sthevanbello <sthevan.alves@gmail.com>
2022-09-11 18:54:33 -0300

    Paciente Repository implementado com os métodos básicos

---
+++ Repositories/PacienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Desafio_EF.Contexts;
+using Desafio_EF.Interfaces;
+using Desafio_EF.Models;
+
+namespace Desafio_EF.Repositories
+{
+    public class PacienteRepository : BaseRepository<Paciente>, IPacienteRepository
+    {
+        public PacienteRepository(DesafioContext desafioContext) : base(desafioContext)
+        {
+        }
+    }
+}
